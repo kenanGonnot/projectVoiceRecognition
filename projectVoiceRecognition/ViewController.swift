aba54dfd13eb102d88792d620ea8e15ddc27910b
kenanGonnot <78441094+kenanGonnot@users.noreply.github.com>
2021-05-23 15:36:39 +0200

    new button "about us"

--- projectVoiceRecognition/ViewController.swift
+++ projectVoiceRecognition/ViewController.swift
@@ -97,7 +97,7 @@ class ViewController: UIViewController {
     //        })
     
     @IBAction func showMessage(sender: UIButton) {
-        let alertController = UIAlertController(title: "Welcome to My First App", message: "Hello World", preferredStyle: UIAlertController.Style.alert)
+        let alertController = UIAlertController(title: "Welcome to our First App", message: "Hello ! This is Kenan and Fayçal !", preferredStyle: UIAlertController.Style.alert)
         alertController.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
         present(alertController, animated: true, completion: nil)
     }
