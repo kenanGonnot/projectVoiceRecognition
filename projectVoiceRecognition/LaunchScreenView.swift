d06130da7df1bc733f837cb7fb488d9c071dfe19
kenanGonnot <78441094+kenanGonnot@users.noreply.github.com>
2021-05-28 17:34:53 +0200

    add LaunchScreen + correction constraints HomePage

---
+++ projectVoiceRecognition/LaunchScreenView.swift
@@ -0,0 +1,20 @@
+//
+//  LaunchScreenView.swift
+//  projectVoiceRecognition
+//
+//  Created by Kenan on 28/05/2021.
+//
+
+import SwiftUI
+
+struct LaunchScreenView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct LaunchScreenView_Previews: PreviewProvider {
+    static var previews: some View {
+        LaunchScreenView()
+    }
+}
